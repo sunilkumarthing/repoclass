f7e78ed730b0c272e42ebdb270b196b017bddbcd
sunilkumarthing <thingsunil100@gmail.com>
2019-01-19 15:29:06 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import sunil from './sunil.jpg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <img src={sunil} height="200px" width="250px" alt=""/>
+          <p>
+            This is Sunil Kumar Thing.
+            I am creating my first react App.
+          </p>
+          <a
+            className="App-link"
+            href="http://bimalparajuli.com.np"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Go to bimalparajuli
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
